eed46745b4de8b310dee8c5db569d38f620c79fb
Dylan Shade <Dylan.Shade@stantec.com>
2023-04-28 10:37:02 -0400

    refactored endpoitns

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,44 +1,123 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
-	"go-scripture/pkg/embeddings"
-	"go-scripture/pkg/similarity"
-	"net/http"
+    "encoding/json"
+    "fmt"
+    "go-scripture/pkg/embeddings"
+    "go-scripture/pkg/similarity"
+    "net/http"
+    "github.com/gorilla/mux"
 )
 
 type Embedding = embeddings.Embedding
 
 // Functions: handleSearch
-func HandleSearch(w http.ResponseWriter, r *http.Request, embeddingsByChapter []Embedding, embeddingsByVerse []Embedding, verseMap map[string]string) {
-	searchBy := r.URL.Query().Get("search_by")
-	query := r.URL.Query().Get("query")
 
-	if searchBy != "" && query != "" {
+func HandleSearchByVerse(w http.ResponseWriter, r *http.Request, embeddingsByVerse []Embedding) {
+    vars := mux.Vars(r)
+    book := vars["book"]
+    chapter := vars["chapter"]
+    verse := vars["verse"]
 
-		found := similarity.FindSimilarities(query, embeddingsByChapter, embeddingsByVerse, verseMap, searchBy)
+    found := similarity.FindSimilarities(fmt.Sprintf("%s %s:%s", book, chapter, verse), nil, embeddingsByVerse, nil, "text")
 
-		if searchBy == "passage" {
-			found = similarity.FindBestPassages(found, 2, 200)
-			found = similarity.MergePassageResults(found, query, verseMap)
-		} else {
-			found = found[:50]
-		}
+    jsonArray := make([]map[string]interface{}, len(found))
+    for i, e := range found {
+        jsonArray[i] = map[string]interface{}{
+            "index":        i,
+            "location":     e.Location,
+            "verse":        e.Verse,
+            "similarities": e.Similarity,
+        }
+    }
 
-		jsonArray := make([]map[string]interface{}, len(found))
-		for i, e := range found {
-			jsonArray[i] = map[string]interface{}{
-				"index":        i,
-				"location":     e.Location,
-				"verse":        e.Verse,
-				"similarities": e.Similarity,
-			}
-		}
-		// fmt.Println(jsonArray)
-		fmt.Printf("Search by: %s, Query: %s", searchBy, query)
-		json.NewEncoder(w).Encode(jsonArray)
-	} else {
-		http.Error(w, "Missing query parameters 'search_by' and 'query'", http.StatusBadRequest)
-	}
+    fmt.Printf("Search by verse: %s %s:%s", book, chapter, verse)
+    json.NewEncoder(w).Encode(jsonArray)
+}
+
+func HandleSearchByChapter(w http.ResponseWriter, r *http.Request, embeddingsByChapter []Embedding) {
+    vars := mux.Vars(r)
+    book := vars["book"]
+    chapter := vars["chapter"]
+
+    found := similarity.FindSimilarities(fmt.Sprintf("%s %s", book, chapter), embeddingsByChapter, nil, nil, "chapter")
+
+    jsonArray := make([]map[string]interface{}, len(found))
+    for i, e := range found {
+        jsonArray[i] = map[string]interface{}{
+            "index":        i,
+            "location":     e.Location,
+            "verse":        e.Verse,
+            "similarities": e.Similarity,
+        }
+    }
+
+    fmt.Printf("Search by chapter: %s %s", book, chapter)
+    json.NewEncoder(w).Encode(jsonArray)
+}
+
+func HandleSearchByPassage(w http.ResponseWriter, r *http.Request, embeddingsByChapter []Embedding, embeddingsByVerse []Embedding, verseMap map[string]string) {
+    vars := mux.Vars(r)
+    book := vars["book"]
+    chapter := vars["chapter"]
+    verseStart := vars["verse"]
+    verseEnd := vars["verse_end"]
+
+    found := similarity.FindSimilarities(fmt.Sprintf("%s %s:%s-%s", book, chapter,  verseStart, verseEnd), embeddingsByChapter, embeddingsByVerse, verseMap, "passage")
+    found = similarity.FindBestPassages(found, 2, 200)
+    found = similarity.MergePassageResults(found, passage, verseMap)
+
+    jsonArray := make([]map[string]interface{}, len(found))
+    for i, e := range found {
+        jsonArray[i] = map[string]interface{}{
+            "index":        i,
+            "location":     e.Location,
+            "verse":        e.Verse,
+            "similarities": e.Similarity,
+        }
+    }
+
+    fmt.Printf("Search by passage: %s", passage)
+    json.NewEncoder(w).Encode(jsonArray)
+}
+
+func HandleQuery(w http.ResponseWriter, r *http.Request, embeddingsByChapter []Embedding, embeddingsByVerse []Embedding, verseMap map[string]string) {
+    vars := mux.Vars(r)
+    searchBy := vars["search_by"]
+    query := vars["query"]
+	
+    if searchBy == "" || query == "" {
+        http.Error(w, "Missing query parameters 'search_by' and 'query'", http.StatusBadRequest)
+        return
+    }
+
+    found := similarity.FindSimilarities(query, embeddingsByChapter, embeddingsByVerse, verseMap, searchBy)
+
+    if searchBy == "passage" {
+        found = similarity.FindBestPassages(found, 2, 200)
+        found = similarity.MergePassageResults(found, query, verseMap)
+    } else {
+        found = found[:50]
+    }
+
+    type SearchResult struct {
+        Index       int                     `json:"index"`
+        Location    string                  `json:"location"`
+        Verse       string                  `json:"verse"`
+        Similarities []similarity.Similarity `json:"similarities"`
+    }
+
+    var searchResults []SearchResult
+    for i, e := range found {
+        searchResults = append(searchResults, SearchResult{
+            Index:          i,
+            Location:       e.Location,
+            Verse:          e.Verse,
+            Similarities:   e.Similarity,
+        })
+    }
+
+    // fmt.Println(jsonArray)
+    fmt.Printf("Search by: %s, Query: %s", searchBy, query)
+    json.NewEncoder(w).Encode(searchResults)
 }
--- pkg/similarity/handleQuery.go
+++ pkg/similarity/handleQuery.go
@@ -1,166 +1,101 @@
 package similarity
 
 import (
-	"context"
-	"fmt"
-	"go-scripture/pkg/embeddings" // package for working with Bible verse embeddings
-	"os"
-	"sort"
-	"strconv"
-	"strings"
-	"sync"
+    "context"
+    "fmt"
+    "go-scripture/pkg/embeddings"
+    "os"
+    "sort"
+    "strconv"
+    "strings"
+    "sync"
 
-	"github.com/joho/godotenv"          // package for working with environment variables
-	"github.com/sashabaranov/go-openai" // package for working with OpenAI API
+    "github.com/joho/godotenv"
+    "github.com/sashabaranov/go-openai"
 )
 
-type Embedding = embeddings.Embedding // define an alias for Embedding struct from embeddings package
+type Embedding = embeddings.Embedding
 type Tuple struct {
-	First  int
-	Second float64
+    First  int
+    Second float64
 }
 
 func FindSimilarities(query string, embeddingsByChapter []Embedding, embeddingsByVerse []Embedding, verseMap map[string]string, searchBy string) []Embedding {
-	embeddings := embeddingsByVerse
-	if searchBy == "chapter" {
-		embeddings = embeddingsByChapter
-	}
+    embeddings := embeddingsByVerse
+    if searchBy == "chapter" {
+        embeddings = embeddingsByChapter
+    }
+    searchTermVector := getSearchVector(query, embeddingsByVerse, searchBy, verseMap)
+	embeddings = calculateEmbeddingSimilarity(embeddings, searchTermVector)
+    handleLocationQuery(searchBy, query, &embeddings)
+    sort.Slice(embeddings, func(i, j int) bool {
+        return embeddings[i].Similarity > embeddings[j].Similarity
+    })
 
-	// create a vector representation of the query using pre-trained embeddings
-	// or using the embeddings of the verse location in the query
-	searchTermVector := make([]float64, len(embeddingsByVerse[0].Embedding))
-	hasLoc, loc := checkIfLocation(query)
-	foundLocalEmbedding := false
-	if hasLoc {
-		switch searchBy {
-		case "verse":
-			foundLocalEmbedding, searchTermVector = getEmbeddingByLocation(loc.Book+" "+strconv.Itoa(loc.Chapter)+":"+strconv.Itoa(loc.Verse), embeddingsByVerse)
-		case "chapter":
-			foundLocalEmbedding, searchTermVector = getEmbeddingByLocation(loc.Book+" "+strconv.Itoa(loc.Chapter), embeddingsByChapter)
-		}
-	}
-
-	if !foundLocalEmbedding {
-		// use OpenAI API to get the embeddings for the query
-		// load API key from environment variable
-		// swap query for passage if it is a valid Bible passage
-		// create an embedding request using the query and pre-trained embeddings model
-		// get the embeddings for the query from the OpenAI API response
-
-		godotenv.Load()
-		apiKey := os.Getenv("OPENAI_API_KEY")
-		client := openai.NewClient(apiKey)
-		fmt.Printf("API key: %s\n", apiKey)
-
-		query = swapQueryForPassage(query, verseMap)
-
-		request := openai.EmbeddingRequest{
-			Input: []string{query},
-			Model: openai.AdaEmbeddingV2,
-		}
-		fmt.Println(request)
-
-		resp, err := client.CreateEmbeddings(context.Background(), request)
-		if err != nil {
-			fmt.Printf("Error creating embeddings: %s", err)
-			panic(err)
-		}
-
-		searchTermVector32 := resp.Data[0].Embedding
-		for i, v := range searchTermVector32 {
-			searchTermVector[i] = float64(v)
-		}
-	}
-
-	numWorkers := 8
-	jobs := make(chan int, len(embeddings))
-	results := make(chan Embedding, len(embeddings))
-	var wg sync.WaitGroup
-	for w := 0; w < numWorkers; w++ {
-		wg.Add(1)
-		go func() {
-			for i := range jobs {
-				embeddings[i].Similarity = cosineSimilarity(embeddings[i].Embedding, searchTermVector)
-				results <- embeddings[i]
-			}
-			wg.Done()
-		}()
-	}
-	for i := range embeddings {
-		jobs <- i
-	}
-	close(jobs)
-	wg.Wait()
-	close(results)
-
-	// sort the embeddings by similarity score in descending order
-	handleLocationQuery(searchBy, query, &embeddings)
-	sort.Slice(embeddings, func(i, j int) bool {
-		return embeddings[i].Similarity > embeddings[j].Similarity
-	})
-
-	return embeddings
+    return embeddings
 }
 
-func FindBestPassages(verses []Embedding, windowSize int, numSequences int) []Embedding {
-	// Sort the verses list by Location and Verse.
-	sort.Slice(verses, func(i, j int) bool {
-		if verses[i].Location == verses[j].Location {
-			return verses[i].Verse < verses[j].Verse
-		}
-		return verses[i].Location < verses[j].Location
-	})
+func calculateEmbeddingSimilarity(embeddings []Embedding, searchTermVector []float64) []Embedding {
+    numWorkers := 8
+    jobs := make(chan int, len(embeddings))
+    results := make(chan Embedding, len(embeddings))
+    var wg sync.WaitGroup
+    for w := 0; w < numWorkers; w++ {
+        wg.Add(1)
+        go func() {
+            for i := range jobs {
+                embeddings[i].Similarity = cosineSimilarity(embeddings[i].Embedding, searchTermVector)
+                results <- embeddings[i]
+            }\n            wg.Done()
+        }()
+    }
+    for i := range embeddings {
+        jobs <- i
+    }
+    close(jobs)
+    wg.Wait()
+    close(results)
+    return embeddings
+}
 
-	var bestSequences []Embedding
-	for i := 0; i < numSequences; i++ {
-		// Iterate over the verses list using a sliding window of size `windowSize`.
-		bestWindow := make([]Embedding, windowSize)
-		bestScore := 0.0
+func getSearchVector(query string, embeddingsByVerse []Embedding, searchBy string, verseMap map[string]string) []float64 {
+    vector := make([]float64, len(embeddingsByVerse[0].Embedding))
+    hasLoc, loc := checkIfLocation(query)
+    foundLocalEmbedding := false
+    if hasLoc {
+        switch searchBy {
+        case "verse":
+            foundLocalEmbedding, vector = getEmbeddingByLocation(loc.Book+" "+strconv.Itoa(loc.Chapter)+":"+strconv.Itoa(loc.Verse), embeddingsByVerse)
+		case: "passage":
+			foundLocalEmbedding, vector = getEmbeddingByLocation(loc.Book+" "+strconv.Itoa(loc.Chapter)+":"+strconv.Itoa(loc.VerseStart)+"-"+strconv.Itoa(loc.VerseEnd), embeddingsByVerse)
+        case "chapter":
+            foundLocalEmbedding, vector = getEmbeddingByLocation(loc.Book+" "+strconv.Itoa(loc.Chapter), embeddingsByChapter)
+        }
+    }
+    if !foundLocalEmbedding {
+        vector = getQueryEmbedding(query)
+    }
+    return vector
+}
 
-		for j := i; j <= len(verses)-windowSize && j >= 0; j += numSequences {
-			window := verses[j : j+windowSize]
-			// Calculate the average similarity score for all Embedding structs in the window.
-			sumScore := 0.0
-			for _, e := range window {
-				sumScore += e.Similarity
-			}
-			avgScore := sumScore / float64(windowSize)
-
-			// Update the best window, score, and start index if a higher score is found.
-			if avgScore > bestScore {
-				copy(bestWindow, window)
-				bestScore = avgScore
-			}
-		}
-
-		// Extract book and chapter from the Location field of the first verse in the best window.
-		bookAndChapter := bestWindow[0].Location[:strings.LastIndex(bestWindow[0].Location, ":")]
-		verseStart := bestWindow[0].Location[strings.LastIndex(bestWindow[0].Location, ":")+1:]
-		verseEnd := bestWindow[len(bestWindow)-1].Location[strings.LastIndex(bestWindow[len(bestWindow)-1].Location, ":")+1:]
-		bestLocation := fmt.Sprintf("%s:%s-%s", bookAndChapter, verseStart, verseEnd)
-
-		// Concatenate verses in the best window.
-		var bestVerse strings.Builder
-		for _, e := range bestWindow {
-			if bestVerse.Len() > 0 {
-				bestVerse.WriteString(" ")
-			}
-			verseNum := e.Location[strings.LastIndex(e.Location, ":")+1:]
-			verseStr := verseNum + " " + e.Verse
-
-			bestVerse.WriteString(verseStr)
-		}
-
-		// Append the best sequence to the list of best sequences.
-		bestSequences = append(bestSequences, Embedding{Location: bestLocation, Verse: bestVerse.String(), Similarity: bestScore})
-	}
-
-	// Sort bestSequences by Similarity in descending order.
-	sort.Slice(bestSequences, func(i, j int) bool {
-		return bestSequences[i].Similarity > bestSequences[j].Similarity
-	})
-
-	return bestSequences
+func getQueryEmbedding(query string) []float64 {
+    godotenv.Load()
+    apiKey := os.Getenv("OPENAI_API_KEY")
+    client := openai.NewClient(apiKey)
+    request := openai.EmbeddingRequest{
+        Input: []string{query},
+        Model: openai.AdaEmbeddingV2,
+    }
+    resp, err := client.CreateEmbeddings(context.Background(), request)
+    if err != nil {
+        fmt.Printf("Error creating embeddings: %s", err)
+        panic(err)
+    }
+    embedding := make([]float64, len(resp.Data[0].Embedding))
+    for i, v := range resp.Data[0].Embedding {
+        embedding[i] = float64(v)
+    }
+    return embedding
 }
 
 func swapQueryForPassage(query string, verseMap map[string]string) string {
