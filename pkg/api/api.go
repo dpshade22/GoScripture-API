eda6e94f3e00b3b731f57327797df44a5c83be80
Dylan Shade <63427984+dpshade22@users.noreply.github.com>
2023-05-27 00:12:27 -0400

    struct over string interface

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -11,6 +11,13 @@ import (
 
 type Embedding = embeddings.Embedding
 
+type SearchOutput struct {
+	Index        int     `json:"index"`
+	Location     string  `json:"location"`
+	Verse        string  `json:"verse"`
+	Similarities float64 `json:"similarities"`
+}
+
 func HandleSearchByVerse(c echo.Context, embeddingsByChapter []Embedding, embeddingsByVerse []Embedding, verseMap map[string]string) error {
 	book := c.QueryParam("book")
 	chapter := c.QueryParam("chapter")
@@ -19,18 +26,18 @@ func HandleSearchByVerse(c echo.Context, embeddingsByChapter []Embedding, embedd
 
 	found := similarity.FindSimilarities(locationQuery, embeddingsByChapter, embeddingsByVerse, verseMap, "verse")
 
-	jsonArray := make([]map[string]interface{}, len(found))
+	var searchResults []SearchOutput
 	for i, e := range found {
-		jsonArray[i] = map[string]interface{}{
-			"index":        i,
-			"location":     e.Location,
-			"verse":        e.Verse,
-			"similarities": e.Similarity,
-		}
+		searchResults = append(searchResults, SearchOutput{
+			Index:        i,
+			Location:     e.Location,
+			Verse:        e.Verse,
+			Similarities: e.Similarity,
+		})
 	}
 
 	fmt.Printf("Search by verse: %s", locationQuery)
-	return c.JSON(http.StatusOK, jsonArray)
+	return c.JSON(http.StatusOK, searchResults)
 }
 
 func HandleSearchByChapter(c echo.Context, embeddingsByChapter []Embedding, embeddingsByVerse []Embedding, verseMap map[string]string) error {
@@ -40,43 +47,43 @@ func HandleSearchByChapter(c echo.Context, embeddingsByChapter []Embedding, embe
 
 	found := similarity.FindSimilarities(locationQuery, embeddingsByChapter, embeddingsByVerse, verseMap, "chapter")
 
-	jsonArray := make([]map[string]interface{}, len(found))
+	var searchResults []SearchOutput
 	for i, e := range found {
-		jsonArray[i] = map[string]interface{}{
-			"index":        i,
-			"location":     e.Location,
-			"verse":        e.Verse,
-			"similarities": e.Similarity,
-		}
+		searchResults = append(searchResults, SearchOutput{
+			Index:        i,
+			Location:     e.Location,
+			Verse:        e.Verse,
+			Similarities: e.Similarity,
+		})
 	}
 
 	fmt.Printf("Search by chapter: %s", locationQuery)
-	return c.JSON(http.StatusOK, jsonArray)
+	return c.JSON(http.StatusOK, searchResults)
 }
 
 func HandleSearchByPassage(c echo.Context, embeddingsByChapter []Embedding, embeddingsByVerse []Embedding, verseMap map[string]string) error {
 	book := c.QueryParam("book")
 	chapter := c.QueryParam("chapter")
-	verseStart := c.QueryParam("verse")
-	verseEnd := c.QueryParam("verse_end")
+	verseStart := c.QueryParam("verseStart")
+	verseEnd := c.QueryParam("verseEnd")
 	locationQuery := fmt.Sprintf("%s %s:%s-%s", book, chapter, verseStart, verseEnd)
 
 	found := similarity.FindSimilarities(locationQuery, embeddingsByChapter, embeddingsByVerse, verseMap, "passage")
 	found = similarity.FindBestPassages(found, 2, 200)
 	found = similarity.MergePassageResults(found, locationQuery, verseMap)
 
-	jsonArray := make([]map[string]interface{}, len(found))
+	var searchResults []SearchOutput
 	for i, e := range found {
-		jsonArray[i] = map[string]interface{}{
-			"index":        i,
-			"location":     e.Location,
-			"verse":        e.Verse,
-			"similarities": e.Similarity,
-		}
+		searchResults = append(searchResults, SearchOutput{
+			Index:        i,
+			Location:     e.Location,
+			Verse:        e.Verse,
+			Similarities: e.Similarity,
+		})
 	}
 
 	fmt.Printf("Search by passage: %s", locationQuery)
-	return c.JSON(http.StatusOK, jsonArray)
+	return c.JSON(http.StatusOK, searchResults)
 }
 
 func HandleQuery(c echo.Context, embeddingsByChapter []Embedding, embeddingsByVerse []Embedding, verseMap map[string]string) error {
@@ -96,20 +103,13 @@ func HandleQuery(c echo.Context, embeddingsByChapter []Embedding, embeddingsByVe
 		found = found[:50]
 	}
 
-	type SearchResult struct {
-		Index      int     `json:"index"`
-		Location   string  `json:"location"`
-		Verse      string  `json:"verse"`
-		Similarity float64 `json:"similarities"`
-	}
-
-	var searchResults []SearchResult
+	var searchResults []SearchOutput
 	for i, e := range found {
-		searchResults = append(searchResults, SearchResult{
-			Index:      i,
-			Location:   e.Location,
-			Verse:      e.Verse,
-			Similarity: e.Similarity,
+		searchResults = append(searchResults, SearchOutput{
+			Index:        i,
+			Location:     e.Location,
+			Verse:        e.Verse,
+			Similarities: e.Similarity,
 		})
 	}
 
